a7f07816704c20d5541aac16e6e709cbf38879f6
shuyin21 <shuyindev@gmail.com>
2021-12-05 20:35:34 +0000

    ...

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
       <h1>Hello world</h1>
+      <h2>This is a website</h2>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from 'styled-components';
+
+const navbar = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default navbar
